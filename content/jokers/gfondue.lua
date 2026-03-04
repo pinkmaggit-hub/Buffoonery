50c06222d17200e9cdad6024f5b4ab8b1a2ca7a2
Pink Maggit <pinkmaggitmtg@gmail.com>
2026-03-04 16:16:41 -0300

    Fix Golden Fondue not expiring when $ = 0
    The intended behavior is for it to expire as soon as you get the last 2 dollars. It was previously only expiring after playing a hand with Golden Fondue saying "earn $0".

--- content/jokers/gfondue.lua
+++ content/jokers/gfondue.lua
@@ -28,30 +28,32 @@ SMODS.Joker {
 	end,
     calculate = function(self, card, context)
 		if context.first_hand_drawn then
-			local eval = function()
-				return G.GAME.current_round.hands_played == 0
-			end
+			local eval = function() return G.GAME.current_round.hands_played == 0 end
 			juice_card_until(card, eval, true)
 		end
-		if context.before and G.GAME.current_round.hands_played == 0 and card.ability.extra.money ~= 0 then
-			return { dollars = card.ability.extra.money }
-		end
-		if context.after and G.GAME.current_round.hands_played == 0 and not context.blueprint then
-			-- Mimics Original Behaviour;
-			if card.ability.extra.money == 0 then
-				G.GAME.pool_flags.gfondue_licked = true
-				expire_card(card) --> functions/expire.lua
+		if G.GAME.current_round.hands_played == 0 then
+			if context.before then
 				return {
-					message = localize('buf_gfondue_licked'),
-					colour = G.C.FILTER
-				}
-			else
-				card.ability.extra.money = math.max(card.ability.extra.money - card.ability.extra.money_mod, 0)
-				return {
-					message = "-$"..card.ability.extra.money_mod,
-					colour = G.C.MONEY
+					dollars = card.ability.extra.money,
+					card = card
 				}
 			end
+			if context.after and not context.blueprint then
+				card.ability.extra.money = card.ability.extra.money - card.ability.extra.money_mod
+				if card.ability.extra.money <= 0 then
+					G.GAME.pool_flags.gfondue_licked = true
+					expire_card(card) --> functions/expire.lua
+					return {
+						message = localize('buf_gfondue_licked'),
+						colour = G.C.FILTER
+					}
+				else
+					return {
+						message = "-"..localize('$')..card.ability.extra.money_mod,
+						colour = G.C.MONEY
+					}
+				end
+			end
 		end
-	end
+    end
 }
